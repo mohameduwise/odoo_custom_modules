8264035c4fefab9c7646f76947832f1073e29620
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-02-12 17:58:08 +0530

    [UPDATE]progress bar updation

--- zehntech_survey_extra_fields/static/src/js/survey_loader.js
+++ zehntech_survey_extra_fields/static/src/js/survey_loader.js
@@ -119,7 +119,8 @@ patch(SurveyForm.prototype, {
         } else if (targetEl.value === "next_skipped") {
             this.submitForm({ nextSkipped: true });
         } else if (targetEl.value === "finish" && !this.options.sessionInProgress) {
-            const $button = this.$el.find('button[type="submit"]');
+        console.log("=======================",el)
+            const $button = this.targetEl.find('button[type="submit"]');
 
         // Prevent double click
         if ($button.prop('disabled')) {
