8169798a124f28c8022679e85217205038efa9fd
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-02-14 01:12:26 +0530

    [UPDATE]deleting default mail template

--- instix_customisations/__manifest__.py
+++ instix_customisations/__manifest__.py
@@ -19,7 +19,7 @@ Designed for enterprise hiring workflows with manual HR control.
     "website": "https://internal.insytx.com/",
 
     "category": "Human Resources",
-    "version": "19.1.4",
+    "version": "19.1.5",
     "license": "LGPL-3",
 
     "depends": [
