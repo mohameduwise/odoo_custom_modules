b4a5433c772f6757d6b8cd32224648d987d08bd6
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-03-02 09:04:34 +0530

    [UPDATE]eagles survey  report

--- instix_customisations/__manifest__.py
+++ instix_customisations/__manifest__.py
@@ -19,7 +19,7 @@ Designed for enterprise hiring workflows with manual HR control.
     "website": "https://internal.insytx.com/",
 
     "category": "Human Resources",
-    "version": "19.1.7",
+    "version": "19.1.9",
     "license": "LGPL-3",
 
     "depends": [
