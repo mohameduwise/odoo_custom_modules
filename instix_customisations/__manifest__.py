676c31da1cba9c38313ec64025b10b14e178ae51
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-03-09 00:22:10 +0530

    [UPDATE]fixed the logo in report

--- instix_customisations/__manifest__.py
+++ instix_customisations/__manifest__.py
@@ -19,7 +19,7 @@ Designed for enterprise hiring workflows with manual HR control.
     "website": "https://internal.insytx.com/",
 
     "category": "Human Resources",
-    "version": "19.1.9",
+    "version": "19.2.0",
     "license": "LGPL-3",
 
     "depends": [
