aaff5d1b655a4dee9aea0c2bdafc070ef1d843c5
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-04-07 11:29:37 +0530

    [UPDATE]adding new quotation

--- instix_customisations/__manifest__.py
+++ instix_customisations/__manifest__.py
@@ -19,7 +19,7 @@ Designed for enterprise hiring workflows with manual HR control.
     "website": "https://internal.insytx.com/",
 
     "category": "Human Resources",
-    "version": "19.2.5",
+    "version": "19.2.7",
     "license": "LGPL-3",
 
     "depends": [
