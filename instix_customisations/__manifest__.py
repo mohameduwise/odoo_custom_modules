4385816edb4a06f8777938c54c465eed8fe6a2e2
Muhd-shuhaife <muhammad.p@pragtech.co.in>
2026-04-10 12:01:51 +0530

    [UPDATE]Added the purchase,rfq,sales order,invoice report with validations accoridng to states

--- instix_customisations/__manifest__.py
+++ instix_customisations/__manifest__.py
@@ -19,7 +19,7 @@ Designed for enterprise hiring workflows with manual HR control.
     "website": "https://internal.insytx.com/",
 
     "category": "Human Resources",
-    "version": "19.3.1",
+    "version": "19.3.2",
     "license": "LGPL-3",
 
     "depends": [
